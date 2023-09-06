651db5f09e6df7cc425032aa9ff0b201ef0f24fd
Sai <109896677+GowthamVukoti@users.noreply.github.com>
2023-09-07 01:42:39 +0530

    feat
    added the gogle authentication and connected the app to the atls

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const session=require("express-session");
 const passport=require("passport");
 const LocalStrategy = require('passport-local');
 const passportLocalMongoose=require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 const app=express()
 app.use(express.static("public"));
@@ -20,7 +22,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 //////////////////////////////////////////////////DATABASE CODE///////////////////////////////////////////////////////
-mongoose.connect('mongodb://localhost/userDB',{
+mongoose.connect('mongodb+srv://admin-gowtham:sai123gowtham@cluster0.vj5i6km.mongodb.net/userDB',{
     useNewUrlParser: true, 
     useUnifiedTopology: true,
     family:4
@@ -30,13 +32,44 @@ mongoose.connect('mongodb://localhost/userDB',{
 
 const userSchema=mongoose.Schema({
     email:String,
-    password:String
+    password:String,
+    googleId:String,
+    username:String,
+    userdisplayName:String,
+    secret:[String]
 });
+////////////////////////////////////////////////////GOOGLE AUTHENTICATION/////////////////////////////////////////////////
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 const User=new mongoose.model("User",userSchema);
+
 passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, cb) {
+    process.nextTick(function() {
+      cb(null, { id: user.id, username: user.username });
+    });
+  });
+  
+  passport.deserializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, user);
+    });
+  });
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+    scope:["email","profile"]
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    // console.log(profile)
+    User.findOrCreate({ googleId: profile.id,username:profile.emails[0].value, userdisplayName:profile.displayName }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 
 
@@ -44,6 +77,13 @@ passport.deserializeUser(User.deserializeUser());
 app.get("/",function(req,res){
     res.render("home");
 })
+app.get("/auth/google", passport.authenticate('google', { scope: ["profile","email"] }));
+app.get("/auth/google/secrets", 
+  passport.authenticate('google', { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
 app.get("/login",function(req,res){
     res.render("login");
 })
@@ -51,16 +91,27 @@ app.get("/register",function(req,res){
     res.render("register");
 })
 app.get("/secrets",function(req,res){
-    if(req.isAuthenticated()){
-        res.render("secrets");
-    }else{
-        res.redirect("/login")
-    }
+  User.find(
+    {"secret":{$ne:null}}
+    ).then(foundUsers=>{
+        if(foundUsers){
+            res.render("secrets",{usersWithSecrets:foundUsers});
+        }
+    }).catch(err=>{
+        console.lod(err);
+    })
 })
 app.get("/logout",function(req,res){
     req.logout(function() {
         res.redirect('/');
     });
+})
+app.get("/submit",function(req,res){
+    if(req.isAuthenticated()){
+        res.render("submit");
+    }else{
+        res.redirect("/login")
+    }  
 })
 
 ///////////////////////////////////////////////////////POST REQUESTS//////////////////////////////////////////////////////////
@@ -99,6 +150,22 @@ app.post("/login",function(req,res){
     })
 })
 
+app.post("/submit",function(req,res){
+    const submittedSecret=req.body.secret;
+    User.findById(req.user.id).then(foundUser=>{
+        if(foundUser){
+            foundUser.secret.push(submittedSecret);
+            foundUser.save().then(
+                function(){
+                    res.redirect("/secrets")
+                }
+            )
+        }
+    }).catch(err=>{
+        res.send(err);
+    })
+
+})
 
 
 
